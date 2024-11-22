8ded39e78e21ea1ac87f595ed27127a380253af3
safali56 <safalib12@gmail.com>
2024-11-21 19:15:33 -0500

    Added required files

---
+++ LAB3B/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace LAB3B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbHairdresser = new System.Windows.Forms.ComboBox();
+            this.serviceBox = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chargedItems = new System.Windows.Forms.GroupBox();
+            this.chargedItm = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.priceBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.totalPrice = new System.Windows.Forms.TextBox();
+            this.serviceBtn = new System.Windows.Forms.Button();
+            this.totalBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.chargedItems.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // cmbHairdresser
+            // 
+            this.cmbHairdresser.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.cmbHairdresser.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbHairdresser.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.cmbHairdresser.FormattingEnabled = true;
+            this.cmbHairdresser.Items.AddRange(new object[] {
+            "Jane Samely",
+            "Laura Renkins",
+            "Pat Johnson",
+            "Ron Chambers",
+            "Sue Pallon"});
+            this.cmbHairdresser.Location = new System.Drawing.Point(6, 29);
+            this.cmbHairdresser.Name = "cmbHairdresser";
+            this.cmbHairdresser.Size = new System.Drawing.Size(121, 21);
+            this.cmbHairdresser.Sorted = true;
+            this.cmbHairdresser.TabIndex = 5;
+            this.cmbHairdresser.SelectedIndexChanged += new System.EventHandler(this.cmbHairdresser_SelectedIndexChanged);
+            // 
+            // serviceBox
+            // 
+            this.serviceBox.FormattingEnabled = true;
+            this.serviceBox.Items.AddRange(new object[] {
+            "Cut",
+            "Wash, Blow-dry, and Style",
+            "Color",
+            "Highlight",
+            "Extension",
+            "Up-do"});
+            this.serviceBox.Location = new System.Drawing.Point(16, 29);
+            this.serviceBox.Name = "serviceBox";
+            this.serviceBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.serviceBox.Size = new System.Drawing.Size(135, 95);
+            this.serviceBox.TabIndex = 6;
+            this.serviceBox.SelectedIndexChanged += new System.EventHandler(this.serviceBox_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbHairdresser);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(171, 145);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Hairdresser";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.serviceBox);
+            this.groupBox2.Location = new System.Drawing.Point(205, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(171, 145);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select A Service";
+            // 
+            // chargedItems
+            // 
+            this.chargedItems.Controls.Add(this.chargedItm);
+            this.chargedItems.Location = new System.Drawing.Point(382, 22);
+            this.chargedItems.Name = "chargedItems";
+            this.chargedItems.Size = new System.Drawing.Size(171, 145);
+            this.chargedItems.TabIndex = 10;
+            this.chargedItems.TabStop = false;
+            this.chargedItems.Text = "ChargedItems";
+            // 
+            // chargedItm
+            // 
+            this.chargedItm.FormattingEnabled = true;
+            this.chargedItm.Location = new System.Drawing.Point(23, 29);
+            this.chargedItm.Name = "chargedItm";
+            this.chargedItm.Size = new System.Drawing.Size(120, 95);
+            this.chargedItm.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.priceBox);
+            this.groupBox4.Location = new System.Drawing.Point(569, 22);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(113, 145);
+            this.groupBox4.TabIndex = 11;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Price";
+            // 
+            // priceBox
+            // 
+            this.priceBox.FormattingEnabled = true;
+            this.priceBox.Location = new System.Drawing.Point(6, 29);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(101, 95);
+            this.priceBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(456, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Total Price:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // totalPrice
+            // 
+            this.totalPrice.Location = new System.Drawing.Point(549, 199);
+            this.totalPrice.Name = "totalPrice";
+            this.totalPrice.Size = new System.Drawing.Size(100, 20);
+            this.totalPrice.TabIndex = 13;
+            // 
+            // serviceBtn
+            // 
+            this.serviceBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.serviceBtn.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serviceBtn.Location = new System.Drawing.Point(180, 258);
+            this.serviceBtn.Name = "serviceBtn";
+            this.serviceBtn.Size = new System.Drawing.Size(121, 39);
+            this.serviceBtn.TabIndex = 14;
+            this.serviceBtn.Text = "Add Service";
+            this.serviceBtn.UseVisualStyleBackColor = false;
+            this.serviceBtn.Click += new System.EventHandler(this.serviceBtn_Click);
+            // 
+            // totalBtn
+            // 
+            this.totalBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.totalBtn.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalBtn.Location = new System.Drawing.Point(307, 258);
+            this.totalBtn.Name = "totalBtn";
+            this.totalBtn.Size = new System.Drawing.Size(121, 39);
+            this.totalBtn.TabIndex = 15;
+            this.totalBtn.Text = "Calculate Total";
+            this.totalBtn.UseVisualStyleBackColor = false;
+            this.totalBtn.Click += new System.EventHandler(this.totalBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.resetBtn.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetBtn.Location = new System.Drawing.Point(434, 258);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(121, 39);
+            this.resetBtn.TabIndex = 16;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = false;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(561, 258);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(121, 39);
+            this.exitBtn.TabIndex = 17;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.totalBtn);
+            this.Controls.Add(this.serviceBtn);
+            this.Controls.Add(this.totalPrice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.chargedItems);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Perfect Cut Hair Saloon";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.chargedItems.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbHairdresser;
+        private System.Windows.Forms.ListBox serviceBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox chargedItems;
+        private System.Windows.Forms.ListBox chargedItm;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListBox priceBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox totalPrice;
+        private System.Windows.Forms.Button serviceBtn;
+        private System.Windows.Forms.Button totalBtn;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ LAB3B/Form1.cs
@@ -0,0 +1,167 @@
+﻿
+using System;
+
+using System.Windows.Forms;
+
+namespace LAB3B
+{
+    public partial class Form1 : Form
+    {
+        private bool hairdresserAdded = false; // Track if hairdresser has been added to Charged Items
+        public Form1()
+        {
+            InitializeComponent();
+            cmbHairdresser.SelectedIndex = 0; // Set the default selection to the first item
+            cmbHairdresser.DropDownStyle = ComboBoxStyle.DropDownList; // Disable editing in ComboBox
+            serviceBtn.Enabled = false;       // Disable Add Service button initially
+            totalBtn.Enabled = false; // Disable Calculate Total button initially
+        }
+
+        /// <summary>
+        /// This method return an int which is price for selected hairdresses 
+        /// </summary>
+        /// <returns>Integer - which is price</returns>
+        private int GetHairdresserPrice()
+        {
+            switch (cmbHairdresser.SelectedIndex) //Selected index is the index the  list box
+            {
+                case 0: return 30; // Jane
+                case 1: return 45; // Pat
+                case 2: return 40; // Ron
+                case 3: return 50; // Sue
+                case 4: return 55; // Laurie
+                default: return 0;
+            }
+        }
+
+        /// <summary>
+        /// This method returns an integer (price of a service) for a selected service
+        /// </summary>
+        /// <param name="selectedIndex">sleected index for  a service</param>
+        /// <returns>Integer - (Price of a service)</returns>
+        private int GetServicePrice(int selectedIndex)
+        {
+            switch (selectedIndex)
+            {
+                case 0: return 30;  // Cut
+                case 1: return 20;  // Wash, blow-dry, and style
+                case 2: return 40;  // Colour
+                case 3: return 50;  // Highlights
+                case 4: return 200; // Extension
+                case 5: return 60;  // Up-do
+                default: return 0;
+            }
+        }
+
+        /// <summary>
+        /// This method checkes to see if hairdresser and service are selected, if a hairdresser has been selected then the program disable the drop down
+        /// and gets the hair dresser price and add service button is enabled,  when a service is selected it gets its price and calculateTotal button is enabled, the services, hairdressser infor are added to chargeBox -listBox while the price is 
+        /// added to priceBox.
+        /// </summary>
+        private void AddService()
+        {
+            if (serviceBox.SelectedIndex >= 0)
+            {
+                int selectedServicePrice = GetServicePrice(serviceBox.SelectedIndex);
+
+                // Add Hairdresser to Charged Items only on first Add Service click
+                if (!hairdresserAdded)
+                {
+                    int hairdresserPrice = GetHairdresserPrice();
+                    chargedItm.Items.Add(cmbHairdresser.SelectedItem.ToString()); // Add Hairdresser name
+                    priceBox.Items.Add(hairdresserPrice.ToString("C")); // Add Hairdresser price
+
+                    hairdresserAdded = true;       // Prevent further additions of hairdresser
+                    cmbHairdresser.Enabled = false; // Disable ComboBox after first use
+                }
+
+                // Add selected service and its price
+                chargedItm.Items.Add(serviceBox.SelectedItem.ToString());
+                priceBox.Items.Add(selectedServicePrice.ToString("C"));
+
+                totalBtn.Enabled = true; // Enable Calculate Total button after adding a service
+            }
+        }
+
+        /// <summary>
+        /// This method calulates the total amount, of the services and hairdressers charge which was selected
+        /// By converting each item in price box to an integer, and displaying it back to the total price label as a string
+        /// </summary>
+        private void CalculateTotalPrice()
+        {
+            int total = 0;
+            foreach (var item in priceBox.Items)
+            {
+                total += int.Parse(item.ToString(), System.Globalization.NumberStyles.Currency); //converts the string to an int and adds a currrency sign
+            }
+            totalPrice.Text = total.ToString("C");
+        }
+
+
+        /// <summary>
+        /// This method resets the form to its initial state
+        /// </summary>
+        private void ResetForm()
+        {
+            cmbHairdresser.SelectedIndex = 0;
+            cmbHairdresser.Enabled = true;
+
+            chargedItm.Items.Clear();
+            priceBox.Items.Clear();
+
+            totalPrice.Text = string.Empty;
+
+            serviceBtn.Enabled = false;
+            totalBtn.Enabled = false;
+
+            hairdresserAdded = false; // Reset the hairdresser added flag
+        }
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbHairdresser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Enable Add Service button only if a service is selected
+            serviceBtn.Enabled = serviceBox.SelectedIndex >= 0;
+        }
+
+        private void serviceBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serviceBtn.Enabled = true; // Enable Add Service button when a service is selected
+        }
+
+        private void chargeBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serviceBtn_Click(object sender, EventArgs e)
+        {
+            AddService();
+        }
+
+        private void totalBtn_Click(object sender, EventArgs e)
+        {
+            CalculateTotalPrice();
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
